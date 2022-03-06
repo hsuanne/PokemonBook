259e1267d1725a6ad47781591915b718e5aed01c
Anne Hsu <anne.hsu@cardinalblue.com>
2022-03-06 14:54:47 +0800

    refactor main

--- app/src/main/java/com/example/pokemonbook/MainActivity.kt
+++ app/src/main/java/com/example/pokemonbook/MainActivity.kt
@@ -16,79 +16,80 @@ class MainActivity : AppCompatActivity() {
     private val pokemonViewModel: PokemonViewModel by viewModels {
         PokemonViewModelFactory((application as PokemonApplication).repository)
     }
+    private lateinit var recyclerViewMain: RecyclerView
+    private lateinit var mainAdapter: MainAdapter
+    private lateinit var defaultTab: TextView
+    private lateinit var attackTab: TextView
+    private lateinit var defenseTab: TextView
+    private lateinit var speedTab: TextView
+    private lateinit var tabArray: ArrayList<TextView>
 
     companion object {
         var pokeL: MutableList<Pokemon> = mutableListOf() //用gson抓到的資料會存進這個List
-        var sort_method: String = "default"
+        var sortMethod: String = "DEFAULT"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView_main: RecyclerView = findViewById(R.id.recyclerView_main)
-        recyclerView_main.layoutManager = LinearLayoutManager(this)
 
-        val adapter = MainAdapter(pokemonViewModel, this)
-        recyclerView_main.adapter = adapter
+        setupRecyclerView()
 
         //第一次執行可以清空DB
 //        pokemonViewModel.deletAll()
-        println("pokemonDB:"+pokemonViewModel.pokeL)
+        println("pokemonDB:" + pokemonViewModel.pokeL)
 //        如果DB沒有資料才fetch
 
         pokemonViewModel.typeTitleList.observe(this) { typeTitleList ->
-                adapter.refresh(typeTitleList)
+            mainAdapter.refresh(typeTitleList)
         }
 
-        val default_tab: TextView = findViewById(R.id.tab_default)
-        val atk_tab: TextView = findViewById(R.id.tab_atk)
-        val def_tab: TextView = findViewById(R.id.tab_def)
-        val spd_tab: TextView = findViewById(R.id.tab_spd)
-        default_tab.setOnClickListener {
-            sort_method = "default"
-            pokemonViewModel.sortByMethod(sort_method)
+        defaultTab = findViewById(R.id.tab_default)
+        attackTab = findViewById(R.id.tab_atk)
+        defenseTab = findViewById(R.id.tab_def)
+        speedTab = findViewById(R.id.tab_spd)
+
+        tabArray = ArrayList()
+        tabArray.add(defaultTab)
+        tabArray.add(attackTab)
+        tabArray.add(defenseTab)
+        tabArray.add(speedTab)
+
+        defaultTab.setOnClickListener {
+            sortMethod = "DEFAULT"
+            pokemonViewModel.sortByMethod(sortMethod)
+            updateTabColor()
         }
-        atk_tab.setOnClickListener {
-            sort_method = "atk"
-            pokemonViewModel.sortByMethod(sort_method)
-            it.setBackgroundColor(resources.getColor(R.color.teal_200))
+        attackTab.setOnClickListener {
+            sortMethod = "ATK"
+            pokemonViewModel.sortByMethod(sortMethod)
+            updateTabColor()
         }
-        def_tab.setOnClickListener {
-            sort_method = "def"
-            pokemonViewModel.sortByMethod(sort_method)
+        defenseTab.setOnClickListener {
+            sortMethod = "DEF"
+            pokemonViewModel.sortByMethod(sortMethod)
+            updateTabColor()
         }
-        spd_tab.setOnClickListener {
-            sort_method = "spd"
-            pokemonViewModel.sortByMethod(sort_method)
+        speedTab.setOnClickListener {
+            sortMethod = "SPD"
+            pokemonViewModel.sortByMethod(sortMethod)
+            updateTabColor()
         }
     }
 
-    fun fetchJson(adapter: MainAdapter) {
-        println("Attempting to Fetch JSON")
+    private fun setupRecyclerView() {
+        recyclerViewMain = findViewById(R.id.recyclerView_main)
+        with(recyclerViewMain){
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            mainAdapter = MainAdapter(pokemonViewModel, this@MainActivity)
+                .apply { adapter = this }
+        }
+    }
 
-        val url =
-            "https://gist.githubusercontent.com/mrcsxsiq/b94dbe9ab67147b642baa9109ce16e44/raw/97811a5df2df7304b5bc4fbb9ee018a0339b8a38"
-        val request = Request.Builder().url(url).build()
-
-        val client = OkHttpClient()
-        //requestCall: 非同步
-        client.newCall(request).enqueue(object : Callback { //callback:做完之後再回傳結果
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute Request")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val body = response?.body?.string()
-                val arr = JSONArray(body) //將資料轉成JSONArray
-                println(arr.length()) //809
-
-                val gson = GsonBuilder().create()
-                val result =
-                    gson.fromJson<List<Pokemon>>(body, object : TypeToken<List<Pokemon>>() {}.type)
-                pokeL = result.toMutableList()
-                println("pokeL: " + pokeL.size) //809
-                pokemonViewModel.insertAll(*result.toTypedArray())
-            }
-        })
+    private fun updateTabColor(){
+        for (tab in tabArray) {
+            if (tab.text == sortMethod) tab.background = resources.getDrawable(R.drawable.selected_tab_bg)
+            else tab.background = resources.getDrawable(R.drawable.unselected_tab_bg)
+        }
     }
 }
